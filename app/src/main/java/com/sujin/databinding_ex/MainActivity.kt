d37656ab594796bf073a8e9f730c6cb86dea1548
SujinHuh <juin483@naver.com>
2022-05-16 10:48:35 +0900

    Log / var && val

--- app/src/main/java/com/sujin/databinding_ex/MainActivity.kt
+++ app/src/main/java/com/sujin/databinding_ex/MainActivity.kt
@@ -2,10 +2,12 @@ package com.sujin.databinding_ex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.sujin.databinding_ex.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +26,14 @@ class MainActivity : AppCompatActivity() {
          }
         */
 
+        val test = "테스트 가자고고고고고고고고고!!!!!"
+        Log.e("MainActivity", test)//오류
+        Log.w("MainActivity", test)//경고
+        Log.i("MainActivity", test)//정보
+        Log.d("MainActivity", test)//디버그
+        Log.v("MainActivity", test)//상세
+
+        print(test)
         //2번 방식 - Databinding
         binding.testBtnId.setOnClickListener {
             Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
